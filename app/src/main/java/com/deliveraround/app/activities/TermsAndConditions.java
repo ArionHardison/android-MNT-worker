eb1cbd0ba70568998348c464d45e2121fa009aab
TamilAppoets <T@mil707>
2018-04-02 15:27:15 +0530

    intial commit

---
+++ app/src/main/java/com/deliveraround/app/activities/TermsAndConditions.java
@@ -0,0 +1,49 @@
+package com.deliveraround.app.activities;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebView;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.deliveraround.app.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
+
+import static com.deliveraround.app.BuildConfigure.BASE_URL;
+
+public class TermsAndConditions extends AppCompatActivity {
+
+    @BindView(R.id.back)
+    ImageView back;
+    @BindView(R.id.title)
+    TextView title;
+    @BindView(R.id.webView)
+    WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_terms_and_conditions);
+        ButterKnife.bind(this);
+        title.setText(getString(R.string.terms_and_conditions));
+        webView.getSettings().setAppCacheEnabled(false);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl(BASE_URL+"terms");
+
+    }
+
+    @OnClick(R.id.back)
+    public void onViewClicked() {
+        onBackPressed();
+    }
+
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
+    }
+}
---
+++ app/src/main/java/com/deliveraround/app/helper/FixturesData.java
@@ -0,0 +1,8 @@
+package com.deliveraround.app.helper;
+
+/**
+ * Created by santhosh@appoets.com on 29-09-2017.
+ */
+
+public class FixturesData {
+}
