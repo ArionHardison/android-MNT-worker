66381b7bdad8cf484b2f13ad0619a60ddc9e5dfd
DELL17 <saranraj@appoets.com>
2018-12-10 20:02:54 +0530

    version installation(package name & marevel changes)

---
+++ app/src/main/java/com/geteat/deliver/BuildConfigure.java
@@ -0,0 +1,11 @@
+package com.geteat.deliver;
+
+/**
+ * Created by Tamil on 9/22/2017.
+ */
+
+public class BuildConfigure {
+
+    public static String BASE_URL="https://geteat.in/";
+
+}
---
+++ app/src/main/java/com/geteat/deliver/fcm/MyFirebaseInstanceIDService.java
@@ -0,0 +1,24 @@
+package com.geteat.deliver.fcm;
+
+/**
+ * Created by jayakumar on 16/02/17.
+ */
+
+
+import android.util.Log;
+
+import com.geteat.deliver.helper.SharedHelper;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
+    private static final String TAG = MyFirebaseInstanceIDService.class.getSimpleName();
+
+    @Override
+    public void onTokenRefresh() {
+        super.onTokenRefresh();
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        SharedHelper.putKey(getApplicationContext(),"device_token",""+refreshedToken);
+        Log.e(TAG,""+refreshedToken);
+    }
+}
