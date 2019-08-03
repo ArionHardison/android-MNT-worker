7d917d27a3f70cc747ef7de59f43689e722336a3
Gobu Csg <gobu@appoets.com>
2019-08-03 19:19:54 +0530

    -Base Installation Done

--- app/src/main/java/com/pakupaku/deliver/CountryPicker/CountryPickerListener.java
+++
@@ -1,8 +0,0 @@
-package com.pakupaku.deliver.CountryPicker;
-
-/**
- * Created by mukesh on 25/04/16.
- */
-public interface CountryPickerListener {
-  public void onSelectCountry(Country country);
-}
--- app/src/main/java/com/pakupaku/deliver/helper/FixturesData.java
+++
@@ -1,8 +0,0 @@
-package com.pakupaku.deliver.helper;
-
-/**
- * Created by santhosh@appoets.com on 29-09-2017.
- */
-
-public class FixturesData {
-}
