ef5fb9574b4cdbca320b33e681e395ea5b6c75bb
Gobu Csg <gobu@appoets.com>
2019-08-14 19:01:18 +0530

    -Initial Commit. -Logo, Theme, Icons, Package Name, Google Service are updated.

---
+++ app/src/main/java/com/tringtwentyfour/deliveryboy/api/APIError.java
@@ -0,0 +1,17 @@
+package com.tringtwentyfour.deliveryboy.api;
+
+/**
+ * Created by Tamil on 9/25/2017.
+ */
+
+public class APIError {
+    private String error;
+
+    public APIError() {
+    }
+
+
+    public String getError() {
+        return error;
+    }
+}
---
+++ app/src/main/java/com/tringtwentyfour/deliveryboy/helper/FixturesData.java
@@ -0,0 +1,8 @@
+package com.tringtwentyfour.deliveryboy.helper;
+
+/**
+ * Created by santhosh@appoets.com on 29-09-2017.
+ */
+
+public class FixturesData {
+}
